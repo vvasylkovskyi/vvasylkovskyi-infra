5b974d6dd71ba86d4d2ed644d433583804b80f2a
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-03 19:00:24 +0100

    Add variable to dns

--- modules/dns/outputs.tf
+++ modules/dns/outputs.tf
@@ -1,11 +1,11 @@
 output "dns_record" {
-  value       = aws_route53_record.record.fqdn
+  value = aws_route53_record.record.fqdn
 }
 
 output "aws_route53_zone_name" {
-    value = aws_route53_zone.main.name
+  value = data.aws_route53_zone.main.name
 }
 
 output "aws_route53_zone_id" {
-    value = aws_route53_zone.main.id
+  value = data.aws_route53_zone.main.id
 }
