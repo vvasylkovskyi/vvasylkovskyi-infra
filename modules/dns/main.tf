7d4f8bd78926101058181ce7e775d730f6b08003
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-03 18:55:47 +0100

    Add variable to dns

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,12 +1,12 @@
 resource "aws_route53_zone" "main" {
-  name = "vvasylkovskyi.com"
+  name = var.route53_zone_id
 }
 
 resource "aws_route53_record" "record" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
-    
+
   alias {
     name                   = var.aws_lb_dns_name
     zone_id                = var.aws_lb_zone_id
--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -2,3 +2,4 @@ variable "domain_name" { type = string }
 variable "dns_record" { type = string }
 variable "aws_lb_zone_id" { type = string }
 variable "aws_lb_dns_name" { type = string }
+variable "route53_zone_id" { type = string }
