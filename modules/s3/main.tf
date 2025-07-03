30064e3153ee15abdc079425cc59325796a3c093
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-03 13:21:00 +0100

    Make public read

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -12,6 +12,20 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = var.is_public ? false : true
 }
 
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowPublicRead"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = ["s3:GetObject"]
+      Resource  = "${aws_s3_bucket.this.arn}/*"
+    }]
+  })
+}
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
 
