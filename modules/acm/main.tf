3af13c7c8af9e63a41871a629ae8fac4c7a9af3b
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-04 13:46:47 +0100

    Fix certificate issue

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,15 +1,14 @@
 
 
 provider "aws" {
-  alias                    = "us_east_1"
-  region                   = "us-east-1"
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
 
 resource "aws_acm_certificate" "cert" {
-  provider                  = aws.us_east_1
-  domain_name               = var.route53_zone
-  validation_method         = "DNS"
-  subject_alternative_names = [var.domain_name]
+  provider          = aws.us_east_1
+  domain_name       = var.domain_name
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -1,3 +1,2 @@
-variable "route53_zone" { type = string }
 variable "domain_name" { type = string }
 variable "aws_route53_zone_id" { type = string }
