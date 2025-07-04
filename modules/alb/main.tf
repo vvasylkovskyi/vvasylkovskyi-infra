a0f613935a67ce1647d29af58172e660489c45fe
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-04 13:36:22 +0100

    Add ALB name

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "portfolio" {
-  name               = "portfolio-alb"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.security_group]
-  subnets            = var.subnets 
+  subnets            = var.subnets
 }
 
 resource "aws_lb_listener" "https" {
@@ -22,7 +22,7 @@ resource "aws_lb_listener" "https" {
 }
 
 resource "aws_lb_target_group" "portfolio" {
-  name     = "portfolio-tg"
+  name     = "${alb_name}-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,11 +1,12 @@
 variable "acm_certificate_arn" { type = string }
 variable "aws_acm_certificate_cert" {
   type = object({
-    arn  = string
-    id   = string
+    arn = string
+    id  = string
   })
 }
 variable "vpc_id" { type = string }
 variable "subnets" { type = list(string) }
 variable "security_group" { type = string }
 variable "ec2_instance_id" { type = string }
+variable "alb_name" { type = string }
