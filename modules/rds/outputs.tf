ef47043e71035e92aa4a98f8708a843685418975
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-11 11:47:32 +0100

    Fix

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,9 +1,9 @@
 output "database_host" {
   description = "The hostname of the RDS instance"
-  value       = aws_db_instance.postgres.address
+  value       = aws_db_instance.database.address
 }
 
 output "database_port" {
   description = "The port of the RDS instance"
-  value       = aws_db_instance.postgres.port
+  value       = aws_db_instance.database.port
 }
