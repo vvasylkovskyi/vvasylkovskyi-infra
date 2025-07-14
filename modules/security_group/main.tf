e7ce678a5f337fc119aa50aba66f23ece04f0cf2
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-14 12:45:04 +0100

    Revert security group to postgresq

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -46,8 +46,8 @@ resource "aws_security_group" "rds" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
   }
     
