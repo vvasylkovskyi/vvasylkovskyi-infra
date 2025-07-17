3d964b750b18decdc0501f9efe345b31a55baed6
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-17 16:49:43 +0100

    Add IOT Thing

--- modules/iot-thing/outputs.tf
+++ modules/iot-thing/outputs.tf
@@ -1,35 +1,23 @@
-resource "aws_iot_thing" "thing" {
-  name = "raspberry-pi-camera"
+
+output "certificate_pem" {
+  value     = aws_iot_certificate.device_cert.certificate_pem
+  sensitive = true
 }
 
-resource "aws_iot_policy" "publish_policy" {
-  name = "RaspberryPiPublishPolicy"
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "iot:Connect",
-          "iot:Publish",
-          "iot:Subscribe",
-          "iot:Receive"
-        ],
-        "Resource": "*"
-      }
-    ]
-  })
+output "private_key" {
+  value     = aws_iot_certificate.device_cert.private_key
+  sensitive = true
 }
 
-resource "aws_iot_certificate" "device_cert" {
-  active = true
-}
-resource "aws_iot_thing_principal_attachment" "attach_cert" {
-  thing       = aws_iot_thing.thing.id
-  principal   = aws_iot_certificate.device_cert.arn
+output "public_key" {
+  value     = aws_iot_certificate.device_cert.public_key
+  sensitive = true
 }
 
-resource "aws_iot_policy_attachment" "attach_policy" {
-  policy     = aws_iot_policy.publish_policy.name
-  target     = aws_iot_certificate.device_cert.arn
+output "certificate_arn" {
+  value = aws_iot_certificate.device_cert.arn
+}
+
+output "iot_endpoint" {
+  value = data.aws_iot_endpoint.iot_endpoint.endpoint_address
 }
