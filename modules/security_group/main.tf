968ab8f5215a000c08ac661328d30137a41ee824
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-23 13:01:34 +0100

    Allow udp

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "ec2" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 4001
+    to_port     = 4001
+    protocol    = "udp"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
