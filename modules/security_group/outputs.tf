454b118f79a04cd0b66af06d8e0571dd2e966e08
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-08-21 13:59:42 +0100

    WIP

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,4 +1,5 @@
 output "security_group_ec2" { value = aws_security_group.ec2.id }
-output "security_group_alb" { value = aws_security_group.alb.id }
+output "security_group_alb_http" { value = aws_security_group.alb_http.id }
+output "security_group_alb_https" { value = aws_security_group.alb_https.id }
 output "security_group_rds" { value = aws_security_group.rds.id }
 
